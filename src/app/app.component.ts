4ca3c0d451f37be66b5e2acad66658c95d73aad0
Rob Hammann <rhammann@vermeer.com>
2024-05-10 14:19:22 -0500

    adding angular and rough template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatIconButton, MatIcon, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
