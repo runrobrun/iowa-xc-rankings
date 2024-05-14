e68b5f74417d41455dd9efc98e7999ad87244acb
SageAAdam <sageadeleadam@gmail.com>
2024-05-13 20:36:18 -0500

    Added a welcome component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,11 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
-
+import { WelcomeComponent} from './welcome/welcome.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatIconButton, MatIcon, RouterLink],
+  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatIconButton, MatIcon, RouterLink, WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
